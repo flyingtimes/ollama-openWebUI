bde5f84e1b42b068f5db0f7a6e13ad4ca652ef81
flyingtimes <5200374@qq.com>
2024-11-10 21:20:46 +0800

    add sh

---
+++ train-from-tensor-to-ollama-model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# trainning using data/ folder 
+HF_ENDPOINT=https://hf-mirror.com mlx_lm.lora --model Qwen/Qwen2.5-72B-Instruct  --data data/ --train --iters 3000 --batch-size 4 --lora-layers 16 --adapter-path adapters/Qwen2.5-72B-$1
+
+# fuse model
+mlx_lm.fuse --model Qwen/Qwen2.5-72B-Instruct --adapter-path adapters/Qwen2.5-72B-FT-$1  --save-path gguf-models/Qwen2.5-72B-$1
+
+# transfer to gguf
+python convert-hf-to-gguf.py gguf-models/Qwen2.5-72B-$1 --outtype q8_0
+
+# copy modelfile
+cp qwen_modelfile.txt gguf-models/Qwen2.5-72B-$1
+# create ollama model
+cd gguf-models/Qwen2.5-72B-$1;ollama create qwen72b-$1 -f qwen_modelfile.txt
